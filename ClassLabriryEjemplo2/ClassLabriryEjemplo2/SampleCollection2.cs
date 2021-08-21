0dca4a0d59ff9c9235548e95ef2c3ebf5834e47c
Calix1402 <88732337+Calix1402@users.noreply.github.com>
2021-08-21 14:42:38 -0600

    proyectos

---
+++ ClassLabriryEjemplo2/ClassLabriryEjemplo2/SampleCollection2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLabriryEjemplo2
+{
+    public class SampleCollection2
+    {
+        static void Main()
+        {
+            string nombre;
+            int num;
+            Console.WriteLine("Jose Benavidez");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Hola {0}, cuantos años tienes?", nombre);
+            num = int.Parse(System.Console.ReadLine());
+            Console.WriteLine("{0} tienes {18} años, eres mayor de edad ", nombre, num);
+            Console.Read();
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ ConsoleAppEejemplo2/ConsoleAppEejemplo2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLabriryEjemplo2;
+
+namespace ConsoleAppEejemplo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el nombre ");
+            Console.WriteLine("1.nombre ");
+            Console.ReadLine();
+
+            Console.WriteLine("Ingresar edad");
+            Console.WriteLine("2.edad");
+            int edad = Convert.ToInt32(Console.ReadLine());
+            if (edad > 18)
+            {
+                Console.WriteLine("ES MAYOR DE EDAD ");
+
+            }
+
+            else
+            {
+                Console.WriteLine("ES MENOR DE EDAD ");
+            }
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
